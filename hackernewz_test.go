f98cff1261e3ad01e45c78264108fc93da763f9a
Jalil <jalilsan@gmail.com>
2015-09-10 21:27:10 -0700

    write test for Comment and NewClient

---
+++ hackernewz_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewClient(t *testing.T) {
+	want := NewClient()
+
+	if want.Version != "v0" {
+		t.Errorf("want nil but got  %v", want)
+	}
+}
+
+func TestGetItem(t *testing.T) {
+	client := NewClient()
+	item := client.GetItem(2921983)
+	fmt.Println(item)
+}
+
+//why is this failing when func is 't' and not T
+func TestGetComment(t *testing.T) {
+	client := NewClient()
+
+	got, err := client.GetComment(2921983)
+
+	if err != nil {
+		t.Errorf("want nil error but got %v", err)
+	}
+
+	want := Comment{
+		By:     "norvig",
+		Id:     2921983,
+		Kids:   []int{2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141},
+		Parent: 2921506,
+		Text:   "Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?",
+		Time:   1314211127,
+	}
+
+	if got.Id != want.Id {
+		t.Error("want got.Id to 2921983 got", got.Id)
+	}
+
+	emptyComment := Comment{}
+	if emptyComment.Id == got.Id {
+		t.Error("WTF! DANGER WILL ROBINSON!")
+	}
+}
